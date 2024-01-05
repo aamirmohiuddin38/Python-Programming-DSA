d47430245a8f94799014a34fa78c5b010c943791
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-05 19:12:54 +0530

    RestaurantBill - formatted print statement

---
+++ RestaurantBill.py
@@ -0,0 +1,31 @@
+# Restaurant Bill Program - exploring print() function
+import os
+
+os.system('cls') #clear console
+
+print("\t\t\t\tZEWA(KFC)")
+print("\t\t\t    KASHMIR FOOD CENTER")
+print("-----------------------------------------------------------------------------")
+
+print("________________________________________________________________________")
+print("| Bill To: \t\t\t\t\t Bill No: A001390\t|")
+print("| AYATHMULLA \t\t\t\t\t Date: 03-01-202\t|")
+print("| 6005899633 \t\t\t TimeStamp: 03-01-2024 05:45:32 PM\t|")
+print("|_______________________________________________________________________|")
+print("_______________________________________________________")
+print("| S.No | \t Description \t | Qty | Rate |\tAmount |")
+print("|______|_________________________|_____|______|________|")
+print("| 1    | BIRYANI CHICKEN HALF \t |  1.0|  130 |     130|")
+print("|      |                         |     |      |        |")
+print("| 2    | DC30                 \t |  1.0|   30 |      30|")
+print("|______|_________________________|_____|______|________|")
+print("|      |                              SubTotal|     160|")
+print("|______|______________________________________|________|")
+    
+print("BANK DETAILS:")
+print("ACC NO: *********0012")
+print("Mobile No: 0002223334")
+print("IFSC: JAKA0KASH")
+
+print("\t\t\t\t Thanks Visit Again ")
+print("-------------------------------------------------------------------------------")
