afd60d102e7078ab690865f3fe7f85a76fbf19bb
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-23 23:29:13 +0530

    constants in python

--- constants.py
+++ constants.py
@@ -1,1 +1,7 @@
+"""
+A constant is a special type of variable whose value cannot be changed.
+In Python, constants are usually declared and assigned in a module 
+(a new file containing variables, functions, etc which is imported to the main file). 
+"""
+
 PI = 3.142
