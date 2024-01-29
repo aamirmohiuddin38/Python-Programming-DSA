1a8bdd85209f2ee9cc8d37f0dba446040842c3c3
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-29 19:07:06 +0530

    if(): statement - checking whether a number is even

---
+++ DecisionMaking.py
@@ -0,0 +1,19 @@
+# Decision Making - IF ELSE ELIF
+import os
+os.system("cls")
+
+print("Python | Programming")
+print("--------------------------------------")
+print("Decision Making Statements....\n")
+
+a = int(input("Enter a: | "))
+
+#? IF STATEMENT - checking whether 'a' is even, if even, print 'a' and if not then do nothing
+print("\n\t -----------------------------------")
+print("\t If(): Statement...")
+print("\t -----------------------------------")
+if(a % 2 == 0):
+    print("\t 'a' is EVEN.")
+
+
+print("\n")
