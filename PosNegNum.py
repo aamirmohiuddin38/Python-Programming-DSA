599365dab7c33eaab3ae2221b64fdfae589b470c
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-02-04 21:26:34 +0530

    whether a num is +ive -ive or zero

---
+++ PosNegNum.py
@@ -0,0 +1,17 @@
+# Check whether a number is positive, negative or zero
+import os
+os.system("cls")
+
+print("Python | Programming")
+print("----------------------------")
+print("Positive | Negative | Zero\n")
+
+num = int(input("Enter num: | "))
+print("\t---------------------------------")
+if num > 0: print("\tPositive")
+elif num < 0: print("\tNegative")
+else: print("\tZero")
+print("\t------------------------------------")
+
+
+print("\n")
