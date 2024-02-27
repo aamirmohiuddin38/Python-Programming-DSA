34d7515e730698ca28a1235a7bf29174624e6899
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-02-27 22:10:56 +0530

    changes in print statement - range in for loop - printing in one line

--- ForLoop.py
+++ ForLoop.py
@@ -9,26 +9,32 @@ print("FOR LOOP")
 print("\n\t Incrementing by 1 - Ascending Order")
 print("\t--------------------------------------")
 for i in range(0,10):
-    print("\t {}".format(i))
+    print("\t",i)
 
 #? Incrementing by 2 - Ascending Order
 print("\n\t Incrementing by 2 - Ascending Order")
 print("\t----------------------------------------")
 for i in range(0,10,2):
-    print("\t {}".format(i))
+    print("\t",i)
 
 #? Decrementing by 1 - Descending Order
 print("\n\t Decrementing by 1 - Descending Order")
 print("\t ------------------------------------------")
 for i in range(10,0, -1):
-    print("\t {}".format(i))
+    print("\t",i)
 
 #? Decrementing by 2 - Descending Order
 print("\n\t Decrementing by 2 - Descending Order")
 print("\t-------------------------------------------")
 for i in range(10,0,-2):
-    print("\t {}".format(i))
+    print("\t",i)
 
-
+#! for in range(a,b,step) - 'a' is included and 'b' is not
+#? Syntax to print numbers on same line
+print("\n\t Print numbers of same line...")
+print("\t---------------------------------------")
+print("\t", end ='')
+for i in range(0,10):
+    print(i , end = " ")
 
 print("\n")
